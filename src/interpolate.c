8468823719837719029861aff99b2ce335601f01
Jonah Joughin <jonah@jough.in>
2020-06-18 11:56:18 -0700

    Formatting

--- src/interpolate.c
+++ src/interpolate.c
@@ -2,80 +2,78 @@
 
 // Equals function with tolerance (to avoid floating point)
 int _soft_eq(double a, double b, double epsilon) {
-    return (a == b) || (a <= b + epsilon && a >= b - epsilon);
+  return (a == b) || (a <= b + epsilon && a >= b - epsilon);
 }
 
 // Bilinear interpolation
 double bilinear(const double* normalized_coord, double val1, const double val2, double val3, double val4) {
-    // Bilinear weights
-    double V[4];
-    // Set up weights
-    V[0] = val1;
-    V[1] = -val1 + val2;
-    V[2] = -val1 + val4;
-    V[3] = val1 - val2 + val3 - val4;
-    // Evaluate
-    return V[0] + V[1] * normalized_coord[0] + V[2] * normalized_coord[1] + V[3] * normalized_coord[0] * normalized_coord[1];
+  // Bilinear weights
+  double V[4];
+  // Set up weights
+  V[0] = val1;
+  V[1] = -val1 + val2;
+  V[2] = -val1 + val4;
+  V[3] = val1 - val2 + val3 - val4;
+  // Evaluate
+  return V[0] + V[1] * normalized_coord[0] + V[2] * normalized_coord[1] + V[3] * normalized_coord[0] * normalized_coord[1];
 }
 
 // Inverse bilinear interpolation
 // Given xy coordinates and the vertices of a quadrilateral, returns the normalized coordinates of xy in [0,1]x[0,1]
-int inverse_bilinear(const double* coord, const double* vec1, const double* vec2, const double* vec3, const double* vec4, double *normalized_coord) {
-    // Bilinear weights
-    double V[4], W[4];
-    // Polynomial variables
-    double a, b, c, det;
-    // Normalized coordinates
-    double xi, eta;
-    // Set up weights
-    V[0] = vec1[0];
-    V[1] = -vec1[0] + vec2[0];
-    V[2] = -vec1[0] + vec4[0];
-    V[3] = vec1[0] - vec2[0] + vec3[0] - vec4[0];
-    W[0] = vec1[1];
-    W[1] = -vec1[1] + vec2[1];
-    W[2] = -vec1[1] + vec4[1];
-    W[3] = vec1[1] - vec2[1] + vec3[1] - vec4[1];
-    // Calculate polynomial coefficients
-    a = V[3] * W[2] - V[2] * W[3];
-    b = V[3] * W[0] - V[0] * W[3] + V[1] * W[2] - V[2]*W[1] + coord[0]*W[3] - coord[1]*V[3];
-    c = V[1] * W[0] - V[0] * W[1] + coord[0]*W[1] - coord[1]*V[1];
+int inverse_bilinear(const double* coord, const double* vec1, const double* vec2, const double* vec3, const double* vec4, double* normalized_coord) {
+  // Bilinear weights
+  double V[4], W[4];
+  // Polynomial variables
+  double a, b, c, det;
+  // Normalized coordinates
+  double xi, eta;
+  // Set up weights
+  V[0] = vec1[0];
+  V[1] = -vec1[0] + vec2[0];
+  V[2] = -vec1[0] + vec4[0];
+  V[3] = vec1[0] - vec2[0] + vec3[0] - vec4[0];
+  W[0] = vec1[1];
+  W[1] = -vec1[1] + vec2[1];
+  W[2] = -vec1[1] + vec4[1];
+  W[3] = vec1[1] - vec2[1] + vec3[1] - vec4[1];
+  // Calculate polynomial coefficients
+  a = V[3] * W[2] - V[2] * W[3];
+  b = V[3] * W[0] - V[0] * W[3] + V[1] * W[2] - V[2] * W[1] + coord[0] * W[3] - coord[1] * V[3];
+  c = V[1] * W[0] - V[0] * W[1] + coord[0] * W[1] - coord[1] * V[1];
 
-    // Solve for xi, eta in [0,1]x[0,1]
+  // Solve for xi, eta in [0,1]x[0,1]
 
-    if (_soft_eq(a, 0, 1e-10)) {
-        // Linear case
-        xi = -c/b;
-    } else {
-        // Quadratic case
-        det = sqrt(b*b - 4*a*c);
-        xi = (-b+det)/(2*a);
-    }
+  if (_soft_eq(a, 0, 1e-10)) {
+    // Linear case
+    xi = -c / b;
+  } else {
+    // Quadratic case
+    det = sqrt(b * b - 4 * a * c);
+    xi = (-b + det) / (2 * a);
+  }
 
-    eta = (coord[0]-V[0]-V[2]*xi)/(V[1]+V[3]*xi);
+  eta = (coord[0] - V[0] - V[2] * xi) / (V[1] + V[3] * xi);
 
-    // Assign normalized coordinates (eta - y, xi - x)
-    normalized_coord[0] = eta;
-    normalized_coord[1] = xi;
+  // Assign normalized coordinates (eta - y, xi - x)
+  normalized_coord[0] = eta;
+  normalized_coord[1] = xi;
 
-    return 0;
+  return 0;
 }
 
 // Inverse bilinear interpolation
 // Given xyz coordinates and the vertices of a cell, returns the normalized coordinates of xyz in [0,1]x[0,1]x[0,1]
 // It is assumed that xy coordinates remain constant in the z direction
 // Not yet completed
-int inverse_trilinear(const double* coord, const double* v1, const double* v2, const double* v3, const double* v4, const double* v5, const double* v6, const double* v7, const double* v8, double *normalized_coord) {
-    
-    double z0, z1;
+int inverse_trilinear(const double* coord, const double* v1, const double* v2, const double* v3, const double* v4, const double* v5, const double* v6, const double* v7, const double* v8, double* normalized_coord) {
+  double z0, z1;
 
-    inverse_bilinear(coord, v1, v2, v3, v4, normalized_coord);
+  inverse_bilinear(coord, v1, v2, v3, v4, normalized_coord);
 
-    z0 = bilinear(normalized_coord, v1[2], v2[2], v3[2], v4[2]);
-    z1 = bilinear(normalized_coord, v5[2], v6[2], v7[2], v8[2]);
+  z0 = bilinear(normalized_coord, v1[2], v2[2], v3[2], v4[2]);
+  z1 = bilinear(normalized_coord, v5[2], v6[2], v7[2], v8[2]);
 
-    normalized_coord[2] = (coord[2] - z0) / (z1 - z0);
+  normalized_coord[2] = (coord[2] - z0) / (z1 - z0);
 
-    return 0;
+  return 0;
 }
-
--- src/mesh.c
+++ src/mesh.c
@@ -1,73 +1,72 @@
 #include <main.h>
 #include <netcdf.h>
+#include <particle.h>
+#include <sort.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <particle.h>
-#include <sort.h>
-
 int z_curve_idx_comparator(const void *a_, const void *b_) {
-    uint64_t *a = (uint64_t *)a_, *b = (uint64_t *)b_;
-    if (*a < *b) {
-        return -1;
-    } else if (*a == *b) {
-        return 0;
-    } else {
-        return 1;
-    }
+  uint64_t *a = (uint64_t *)a_, *b = (uint64_t *)b_;
+  if (*a < *b) {
+    return -1;
+  } else if (*a == *b) {
+    return 0;
+  } else {
+    return 1;
+  }
 }
 
 int build_mesh(int grd_ncid, int avg_ncid, struct Mesh *mesh) {
+  int y_psi_id, x_psi_id, y_rho_id, x_rho_id, mask_rho_id, y_u_id, x_u_id, y_v_id, x_v_id;
+  int u_id, v_id;
 
-    int y_psi_id, x_psi_id, y_rho_id, x_rho_id, mask_rho_id, y_u_id, x_u_id, y_v_id, x_v_id;
-    int u_id, v_id;
+  size_t u_start[4] = {0, 0, 0, 0};
+  size_t u_end[4] = {1, 1, NY_U, NX_U};
+  size_t v_start[4] = {0, 0, 0, 0};
+  size_t v_end[4] = {1, 1, NY_V, NX_V};
+  // Get grid variable ids
+  CHECK_NC_NOERR(nc_inq_varid(grd_ncid, "y_psi", &y_psi_id));
+  CHECK_NC_NOERR(nc_inq_varid(grd_ncid, "x_psi", &x_psi_id));
+  CHECK_NC_NOERR(nc_inq_varid(grd_ncid, "y_rho", &y_rho_id));
+  CHECK_NC_NOERR(nc_inq_varid(grd_ncid, "x_rho", &x_rho_id));
+  CHECK_NC_NOERR(nc_inq_varid(grd_ncid, "mask_rho", &mask_rho_id));
+  CHECK_NC_NOERR(nc_inq_varid(grd_ncid, "y_u", &y_u_id));
+  CHECK_NC_NOERR(nc_inq_varid(grd_ncid, "x_u", &x_u_id));
+  CHECK_NC_NOERR(nc_inq_varid(grd_ncid, "y_v", &y_v_id));
+  CHECK_NC_NOERR(nc_inq_varid(grd_ncid, "x_v", &x_v_id));
+  // Get u, v, variable ids
+  CHECK_NC_NOERR(nc_inq_varid(avg_ncid, "u", &u_id));
+  CHECK_NC_NOERR(nc_inq_varid(avg_ncid, "v", &v_id));
+  // Copy grid arrays
+  CHECK_NC_NOERR(nc_get_var_double(grd_ncid, y_psi_id, &(mesh->y_psi[0][0])));
+  CHECK_NC_NOERR(nc_get_var_double(grd_ncid, x_psi_id, &(mesh->x_psi[0][0])));
+  CHECK_NC_NOERR(nc_get_var_double(grd_ncid, y_rho_id, &(mesh->y_rho[0][0])));
+  CHECK_NC_NOERR(nc_get_var_double(grd_ncid, x_rho_id, &(mesh->x_rho[0][0])));
+  CHECK_NC_NOERR(nc_get_var_double(grd_ncid, mask_rho_id, &(mesh->mask_rho[0][0])));
+  CHECK_NC_NOERR(nc_get_var_double(grd_ncid, y_u_id, &(mesh->y_u[0][0])));
+  CHECK_NC_NOERR(nc_get_var_double(grd_ncid, x_u_id, &(mesh->x_u[0][0])));
+  CHECK_NC_NOERR(nc_get_var_double(grd_ncid, y_v_id, &(mesh->y_v[0][0])));
+  CHECK_NC_NOERR(nc_get_var_double(grd_ncid, x_v_id, &(mesh->x_v[0][0])));
+  // Copy u, v arrays
+  CHECK_NC_NOERR(nc_get_vara_double(avg_ncid, u_id, u_start, u_end, &(mesh->u[0][0])));
+  CHECK_NC_NOERR(nc_get_vara_double(avg_ncid, v_id, v_start, v_end, &(mesh->v[0][0])));
 
-    size_t u_start[4] = {0, 0, 0, 0};
-    size_t u_end[4] = {1, 1, NY_U, NX_U};
-    size_t v_start[4] = {0, 0, 0, 0};
-    size_t v_end[4] = {1, 1, NY_V, NX_V};
-    // Get grid variable ids
-    CHECK_NC_NOERR(nc_inq_varid(grd_ncid, "y_psi", &y_psi_id));
-    CHECK_NC_NOERR(nc_inq_varid(grd_ncid, "x_psi", &x_psi_id));
-    CHECK_NC_NOERR(nc_inq_varid(grd_ncid, "y_rho", &y_rho_id));
-    CHECK_NC_NOERR(nc_inq_varid(grd_ncid, "x_rho", &x_rho_id));
-    CHECK_NC_NOERR(nc_inq_varid(grd_ncid, "mask_rho", &mask_rho_id));
-    CHECK_NC_NOERR(nc_inq_varid(grd_ncid, "y_u", &y_u_id));
-    CHECK_NC_NOERR(nc_inq_varid(grd_ncid, "x_u", &x_u_id));
-    CHECK_NC_NOERR(nc_inq_varid(grd_ncid, "y_v", &y_v_id));
-    CHECK_NC_NOERR(nc_inq_varid(grd_ncid, "x_v", &x_v_id));
-    // Get u, v, variable ids
-    CHECK_NC_NOERR(nc_inq_varid(avg_ncid, "u", &u_id));
-    CHECK_NC_NOERR(nc_inq_varid(avg_ncid, "v", &v_id));
-    // Copy grid arrays
-    CHECK_NC_NOERR(nc_get_var_double(grd_ncid, y_psi_id, &(mesh->y_psi[0][0])));
-    CHECK_NC_NOERR(nc_get_var_double(grd_ncid, x_psi_id, &(mesh->x_psi[0][0])));
-    CHECK_NC_NOERR(nc_get_var_double(grd_ncid, y_rho_id, &(mesh->y_rho[0][0])));
-    CHECK_NC_NOERR(nc_get_var_double(grd_ncid, x_rho_id, &(mesh->x_rho[0][0])));
-    CHECK_NC_NOERR(nc_get_var_double(grd_ncid, mask_rho_id, &(mesh->mask_rho[0][0])));
-    CHECK_NC_NOERR(nc_get_var_double(grd_ncid, y_u_id, &(mesh->y_u[0][0])));
-    CHECK_NC_NOERR(nc_get_var_double(grd_ncid, x_u_id, &(mesh->x_u[0][0])));
-    CHECK_NC_NOERR(nc_get_var_double(grd_ncid, y_v_id, &(mesh->y_v[0][0])));
-    CHECK_NC_NOERR(nc_get_var_double(grd_ncid, x_v_id, &(mesh->x_v[0][0])));
-    // Copy u, v arrays
-    CHECK_NC_NOERR(nc_get_vara_double(avg_ncid, u_id, u_start, u_end, &(mesh->u[0][0])));
-    CHECK_NC_NOERR(nc_get_vara_double(avg_ncid, v_id, v_start, v_end, &(mesh->v[0][0])));
-
-    uint64_t **idx = (uint64_t **)calloc(NX_RHO * NY_RHO * NZ_RHO, sizeof(uint64_t *));
-
-    for (int i = 0; i < NX_RHO; i++) {
-        for (int j=0; j < NY_RHO; j++) {
-            for (int k=0; k < NZ_RHO; k++) {
-                mesh->z_curve_idx_rho[k][j][i] = _xyz_to_z_curve(i, j, k);
-                idx[k + j*NZ_RHO + i * NY_RHO*NZ_RHO] = &mesh->z_curve_idx_rho[k][j][i];
-            }
-        }
+  uint64_t **idx = (uint64_t **)calloc(NX_RHO * NY_RHO * NZ_RHO, sizeof(uint64_t *));
+  // Get interleaved z_curve codes
+  for (int i = 0; i < NX_RHO; i++) {
+    for (int j = 0; j < NY_RHO; j++) {
+      for (int k = 0; k < NZ_RHO; k++) {
+        mesh->z_curve_idx_rho[k][j][i] = _xyz_to_z_curve(i, j, k);
+        idx[k + j * NZ_RHO + i * NY_RHO * NZ_RHO] = &mesh->z_curve_idx_rho[k][j][i];
+      }
     }
+  }
+  // Sort codes and reindex
+  // This eliminates large number of unused codes, which speeds up sorting by an order of magnitude
+  merge_sort((void **)idx, z_curve_idx_comparator, 0, NX_RHO * NY_RHO * NZ_RHO - 1);
+  for (int i = 0; i < NX_RHO * NY_RHO * NZ_RHO; i++) {
+    *(idx[i]) = i;
+  }
 
-    merge_sort((void **)idx, z_curve_idx_comparator, 0, NX_RHO * NY_RHO * NZ_RHO - 1);
-    for (int i = 0; i < NX_RHO * NY_RHO * NZ_RHO; i++) {
-        *(idx[i]) = i;
-    }
-    
-    return 0;
+  return 0;
 }
